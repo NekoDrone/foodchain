b8c67e00bcef168e0a79f1fd09fc40af2c5b19b4
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-09-22 22:35:45 +0800

    refactor: remove redundant params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 import { MealPlanner } from "@/components/MealPlanner";
 
 const Home = () => {
-  const searchParams = useSearchParams();
-
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <MealPlanner />
