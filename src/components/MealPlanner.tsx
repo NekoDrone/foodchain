fdebb5535cbdf175ab1e26e53bee18d321c4dee8
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-09-22 22:12:22 +0800

    feat: planner component state handling

--- src/components/MealPlanner.tsx
+++ src/components/MealPlanner.tsx
@@ -1,12 +1,31 @@
 import { Plate } from "@/components/Plate/Plate";
-import { CategoryContainer } from "@/components/CategoryContainer/CategoryContainer";
+import { SelectionContainer } from "@/components/CategoryContainer/SelectionContainer";
+import { FoodData, FoodType } from "@/shared/entities";
+import { useState } from "react";
+
+type SelectedFoodState = {
+  [value in FoodType]: FoodData | undefined;
+};
 
 export const MealPlanner = () => {
+  const initialSelected = Object.values(FoodType).reduce((acc, key) => {
+    acc[key] = undefined;
+    return acc;
+  }, {} as SelectedFoodState);
+
+  const [selectedFoods, setSelectedFoods] = useState(initialSelected);
+
+  const addFoodToSelection = (food: FoodData) => {
+    const newState = structuredClone(selectedFoods);
+    newState[food.category] = food;
+    setSelectedFoods(newState);
+  };
+
   return (
-    <div className="flex w-auto justify-center gap-64 align-middle">
+    <div className="flex min-h-fit w-auto justify-center gap-64 align-middle">
       <Plate />
-      <div className="h-750 grid auto-rows-max grid-rows-3 justify-between align-middle">
-        <CategoryContainer />
+      <div className="grid max-h-fit w-[200px] auto-rows-max grid-rows-3 justify-between align-middle">
+        <SelectionContainer handleFoodSelection={addFoodToSelection} />
       </div>
     </div>
   );
