024de8022b6019b0c0c8d919e7bb5014c2e31706
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-09-26 05:32:34 +0800

    feat: nutrition info enums

--- src/shared/entities.ts
+++ src/shared/entities.ts
@@ -15,4 +15,35 @@ export interface FoodData {
   imageLink?: string;
   isHealthy: boolean;
   category: FoodType;
+  nutritionInfo: NutritionInfo;
+}
+
+export interface NutritionInfo {
+  calories: number;
+  fat: number;
+  protein: number;
+  calcium: number;
+  iron: number;
+  carbohydrates: number;
+  sugar: number;
+}
+
+export enum NutritionTypes {
+  Calories = "calories",
+  Fat = "fat",
+  Protein = "protein",
+  Calcium = "calcium",
+  Iron = "iron",
+  Carbohydrates = "carbohydrates",
+  Sugar = "sugar",
+}
+
+export enum NutritionTypesUnit {
+  calories = "kcal",
+  fat = "g",
+  protein = "g",
+  carbohydrates = "g",
+  calcium = "mg",
+  iron = "mg",
+  sugar = "g",
 }
