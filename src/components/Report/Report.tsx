64b3a245f3d768b219142cf2980a1a676eef7a46
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-09-26 05:33:44 +0800

    refactor: adjust styling

--- src/components/Report/Report.tsx
+++ src/components/Report/Report.tsx
@@ -3,6 +3,7 @@
 import { FC } from "react";
 import { NotificationFoodData } from "@/shared/functions/api";
 import { ReportPlate } from "@/components/Report/ReportPlate";
+import { NutritionReadout } from "@/components/NutritionReadout/NutritionReadout";
 
 interface ReportProps {
   selectedFoods: NotificationFoodData;
@@ -13,5 +14,10 @@ export const Report: FC<ReportProps> = ({ selectedFoods }) => {
     <div className="flex min-h-fit min-w-fit items-center justify-center gap-16 align-middle">
       <ReportPlate selectedFoods={selectedFoods} />
     </div>
+    <>
+      <div className="grid min-h-fit min-w-fit items-center justify-center gap-16 align-middle">
+        <ReportPlate selectedFoods={selectedFoods} />
+      </div>
+    </>
   );
 };
--- src/components/Report/ReportPlate.tsx
+++ src/components/Report/ReportPlate.tsx
@@ -7,40 +7,40 @@ interface ReportPlateProps {
   selectedFoods: NotificationFoodData;
 }
 
-const IMAGE_SIZE = 240;
+const IMAGE_SIZE = 275;
 
 export const ReportPlate: FC<ReportPlateProps> = ({ selectedFoods }) => {
   const { carbs, fruitVeg, protein } = selectedFoods;
 
   return (
-    <div className="relative">
-      <div className="absolute inset-0">
-        {carbs && (
-          <FoodSlot
-            food={carbs}
-            className="absolute left-[35%] top-[30%] z-20 scale-75"
-          />
-        )}
-        {fruitVeg && (
-          <FoodSlot
-            food={fruitVeg}
-            className="absolute left-[-15%] top-[15%] z-10 scale-[55%]"
-          />
-        )}
-        {protein && (
-          <FoodSlot
-            food={protein}
-            className="absolute left-[25%] top-[10%] z-0 scale-75"
-          />
-        )}
-      </div>
+    <div className="relative pt-8">
       <Image
         src="/assets/images/plate.webp"
         alt="plate"
         height={IMAGE_SIZE}
         width={IMAGE_SIZE}
-        className="-z-10"
+        className="-z-10 h-fit w-fit"
       />
+      <div className="absolute inset-0 h-auto overflow-visible">
+        {carbs && (
+          <FoodSlot
+            food={carbs}
+            className="absolute right-[-15%] top-[25%] z-20 scale-[60%]"
+          />
+        )}
+        {fruitVeg && (
+          <FoodSlot
+            food={fruitVeg}
+            className="absolute left-[-10%] top-[32%] z-10 scale-75"
+          />
+        )}
+        {protein && (
+          <FoodSlot
+            food={protein}
+            className="absolute left-[23%] top-[10%] z-0 scale-75"
+          />
+        )}
+      </div>
     </div>
   );
 };
