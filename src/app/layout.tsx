ddf956440c465355f4531270b956b1f24b2c3440
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-09-26 07:04:47 +0800

    refactor, style: use different font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import "./globals.css";
-import { Mandali } from "next/font/google";
+import { Inter } from "next/font/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode } from "react";
 
-const mandali = Mandali({ weight: "400", subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 const queryClient = new QueryClient();
 
@@ -21,7 +21,7 @@ const RootLayout = ({
         <title>Masak!</title>
       </head>
       <body
-        className={`${mandali.className} bg-yellow-400 text-gray-900 antialiased`}
+        className={`${inter.className} bg-yellow-400 text-gray-900 antialiased`}
       >
         <QueryClientProvider client={queryClient}>
           {children}
