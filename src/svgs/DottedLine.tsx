79730d2b5e44feb88252af5b0d442c268909b9c4
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-09-26 05:32:47 +0800

    feat: dotted line svg component

---
+++ src/svgs/DottedLine.tsx
@@ -0,0 +1,13 @@
+export const DottedLine = () => {
+  return (
+    <svg
+      width="99"
+      height="1"
+      viewBox="0 0 99 1"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line y1="0.5" x2="99" y2="0.5" stroke="black" strokeDasharray="2 2" />
+    </svg>
+  );
+};
