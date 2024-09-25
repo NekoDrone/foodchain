eb1c81e1dc1d3168c0d8b605e8dcb0de56147a9b
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-09-26 05:34:27 +0800

    fix: provide type to state hook

--- src/shared/functions/sessionStorage.ts
+++ src/shared/functions/sessionStorage.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const useSessionStorage = (key: string) => {
-  const [value, setValue] = useState("{}");
+  const [value, setValue] = useState<string>("{}");
 
   useEffect(() => {
     setValue(sessionStorage.getItem(key) ?? "");
