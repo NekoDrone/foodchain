c9b1b3d23e79703878207b46283fc40c0462cee5
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-09-28 10:44:55 +0800

    feat: add back button to beginning for demo

--- src/app/remind/confirm/page.tsx
+++ src/app/remind/confirm/page.tsx
@@ -1,8 +1,16 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+
 const RemindConfirmPage = () => {
+  
+  const router = useRouter()
+  
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-10 align-middle">
       <h1 className="text-8xl font-bold">Thank you!</h1>
       <p className="text-xl">We&apos;ll send you a reminder in a few days!</p>
+      <button className="bg-white w-50 h-50 opacity-0" onClick={() => router.replace("/")}>Back to beginning</button>
     </div>
   );
 };
--- src/app/viewReport/page.tsx
+++ src/app/viewReport/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { ReportWithNotif } from "@/components/Report/ReportWithNotif";
 import { ReportFromStorage } from "@/components/Report/ReportFromStorage";
 import { Suspense } from "react";
@@ -17,11 +17,15 @@ const ReportPage = () => {
 };
 
 const ReportSuspense = () => {
+
+  const router = useRouter()
+  
   return (
     <Suspense>
       <div className="flex min-h-screen flex-col items-center justify-center align-middle scale-[88%]">
         <ReportPage />
       </div>
+      <button className="bg-white w-50 h-50 opacity-0" onClick={() => router.replace("/")}>Back to beginning</button>
     </Suspense>
   );
 };
